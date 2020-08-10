30513fdb2d67d6dd93948aa2f8d14440c48edf84
LakkamDev <rangaswamy.lakkam@gmail.com>
2020-08-10 23:50:10 +0530

    Thread programs

--- LakkamPrograms/src/com/lakkam/threads/TestSynchronization.java
+++ LakkamPrograms/src/com/lakkam/threads/TestSynchronization.java
@@ -1,9 +1,42 @@
 package com.lakkam.threads;
 
+class MyRunnable implements Runnable{
+	
+	private int balance = 20;
+	@Override
+	public void run() {
+		withDrawAmount(10);
+		
+	}
+
+	private synchronized void withDrawAmount(int amount) {
+		if(amount<=balance) {
+			System.out.println("Withdrawing amount is :"+amount+" : With : "+MyThread.currentThread().getName());
+			balance = balance-amount;
+			System.out.println("Balance after successful withdraw : "+balance+" : With : "+MyThread.currentThread().getName());
+		}
+		else {
+			System.out.println("Insufficient Funds : Remaining Balance"+balance+" : With : "+MyThread.currentThread().getName());
+		}
+		
+	}
+	
+}
 public class TestSynchronization {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	MyRunnable myRunnableObject = new MyRunnable();
+	Thread myThread1 = new Thread(myRunnableObject);
+	myThread1.setName("Lakkam");
+	myThread1.start();
+	
+	Thread myThread2 = new Thread(myRunnableObject);
+	myThread2.setName("Nani");
+	myThread2.start();
+	
+	Thread myThread3 = new Thread(myRunnableObject);
+	myThread3.setName("Prachu");
+	myThread3.start();
 
 	}
 
--- LakkamPrograms/src/com/lakkam/threads/ThreadDemo.java
+++ LakkamPrograms/src/com/lakkam/threads/ThreadDemo.java
@@ -1,9 +1,67 @@
 package com.lakkam.threads;
 
+class MyThread extends Thread {
+
+	private int totalMenEmployees;
+	private int totalWomenEmployees;
+	private int totalEmployeeCount;
+
+	public int getTotalEmployeeCount() throws InterruptedException {
+		MyThread.currentThread().sleep(1000);
+		return totalWomenEmployees + totalMenEmployees;
+	}
+
+	public int getTotalMenEmployees() {
+		return totalMenEmployees;
+	}
+
+	public void setTotalMenEmployees(int totalMenEmployees) {
+		this.totalMenEmployees = totalMenEmployees;
+	}
+
+	public int getTotalWomenEmployees() {
+		return totalWomenEmployees;
+	}
+
+	public void setTotalWomenEmployees(int totalWomenEmployees) {
+		this.totalWomenEmployees = totalWomenEmployees;
+	}
+
+	@Override
+	public void run() {
+		// getTotalMenEmployees(totalMenEmployees, totalWomenEmployees);
+
+		try {
+			totalEmployeeCount = getTotalEmployeeCount();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(
+				" run method...with .." + MyThread.currentThread().getName() + "..." + totalEmployeeCount);
+
+	}
+
+}
+
 public class ThreadDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyThread threadObject = new MyThread();
+		threadObject.setName("First Thread");
+
+		MyThread threadObject2 = new MyThread();
+		threadObject2.setName("Second Thread");
+
+		threadObject.setTotalMenEmployees(2000);
+		threadObject.setTotalWomenEmployees(3000);
+
+		threadObject2.setTotalMenEmployees(500);
+		threadObject2.setTotalWomenEmployees(200);
+		
+		threadObject.start();
+		threadObject2.start();
 
 	}
 
